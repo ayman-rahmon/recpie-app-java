7f7d48e3420640704aca08ab06b507780bd44475
Ayman Rahmon <ayman_rahmon@hotmail.com>
2022-01-15 00:37:12 +0300

    initial commit...

---
+++ app/src/main/java/com/tatsujin/recipe/requests/RecipeApi.java
@@ -0,0 +1,7 @@
+package com.tatsujin.recipe.requests;
+
+public interface RecipeApi {
+
+
+
+}
---
+++ app/src/main/java/com/tatsujin/recipe/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.tatsujin.recipe.utils;
+
+public class Constants {
+
+    // food to fork urls...
+    public static String BASE_URL =  "" ;
+    public static String API_KEY = "" ;
+
+
+
+
+}
